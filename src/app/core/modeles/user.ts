1584b5bcff16bf6351778b8b9348bf3ece706ce2
Skeurz <anas.retro@outlook.com>
2023-05-29 15:18:47 +0000

    v1.0.1 "Added modification / creation date AdmPag"

--- src/app/core/modeles/user.ts
+++ src/app/core/modeles/user.ts
@@ -1,8 +1,10 @@
 export class User {
+    dateCreated!: Date;
     id!:number;
     nom!: string;
     userName!: string ;
     prenom!: string; 
     email!:string;
     password!:string;
+    lastModified!: Date;
 }
