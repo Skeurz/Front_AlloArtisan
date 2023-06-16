9a487323c57848619eb5d1480d5213bf4d4e2895
Skeurz <anas.retro@outlook.com>
2023-06-16 21:43:41 +0100

    v1.3

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -1,20 +1,44 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, CanActivateChild, CanDeactivate, CanLoad, CanMatch, Route, RouterStateSnapshot, UrlSegment, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import { AuthentificationServiceService } from './core/services/authentification-service.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate, CanActivateChild, CanDeactivate<unknown>, CanLoad, CanMatch {
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private authService: AuthentificationServiceService) {}
 
 
-  canActivate(
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+    if (state.url === '/login' || state.url === '/authentifier') {
+      if (this.authService.isLoggedIn()) {
+        this.router.navigate(['/profil']); /* Si l'utilisateur est connecté et essaye d'acceder a la page login 
+        ou inscription il va se rediriger vers sa page profil */
+
+        return false; // Bloque l'accès
+      } else {
+        return true; // Permet l'accès
+      }
+
+    } 
+    else {
+      if (this.authService.isLoggedIn()) {
+        return true; 
+      } else {
+        this.router.navigate(['/login']); /* Redirige les utilisateurs non connecté à l'interface de 
+        connexion s'ils essayent d'acceder a /profil */
+
+        return false; // Bloque l'accès
+      }
+    }
+  }
+ /* canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
     return true;
-  }
+  }*/
   canActivateChild(
     childRoute: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
